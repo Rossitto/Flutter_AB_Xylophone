0be0024781a1a368cbc4dc0952459140b6418dc7
Lucas Schroeder Rossi <rossittao@MacBook-Pro.local>
2020-04-21 18:54:45 -0300

    1 FlatButton for each musical note

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,69 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.lightBlue[100],
         body: SafeArea(
           child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('nqote1.wav');
-              },
-              child: Card(color: Colors.red),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note1.wav');
+                  },
+                  color: Colors.red,
+                  child: null,
+                ),
+                FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note2.wav');
+                  },
+                  color: Colors.orange,
+                  child: null,
+                ),
+                FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note3.wav');
+                  },
+                  color: Colors.yellow,
+                  child: null,
+                ),
+                FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note4.wav');
+                  },
+                  color: Colors.green,
+                  child: null,
+                ),
+                FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note5.wav');
+                  },
+                  color: Colors.greenAccent[700],
+                  child: null,
+                ),
+                FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note6.wav');
+                  },
+                  color: Colors.lightBlue,
+                  child: null,
+                ),
+                FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note7.wav');
+                  },
+                  color: Colors.purple,
+                  child: null,
+                ),
+              ],
             ),
           ),
         ),
