ac1ce4a418db304ccf7c62cdbb9a33f1439b63ec
Lucas Schroeder Rossi <rossittao@MacBook-Pro.local>
2020-04-21 18:43:12 -0300

    audio playing is working!

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -8,7 +10,15 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('nqote1.wav');
+              },
+              child: Card(color: Colors.red),
+            ),
+          ),
         ),
       ),
     );
