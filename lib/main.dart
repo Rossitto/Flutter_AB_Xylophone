6dc96b742971aa4413127a88d7d50d179690f1de
Lucas Schroeder Rossi <rossittao@MacBook-Pro.local>
2020-04-22 09:47:10 -0300

    Removed Center() widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,75 +16,73 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      playSound(1);
-                    },
-                    color: Colors.red,
-                    child: null,
-                  ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  color: Colors.red,
+                  child: null,
                 ),
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      playSound(2);
-                    },
-                    color: Colors.orange,
-                    child: null,
-                  ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  color: Colors.orange,
+                  child: null,
                 ),
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      playSound(3);
-                    },
-                    color: Colors.yellow,
-                    child: null,
-                  ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  color: Colors.yellow,
+                  child: null,
                 ),
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      playSound(4);
-                    },
-                    color: Colors.green,
-                    child: null,
-                  ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  color: Colors.green,
+                  child: null,
                 ),
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      playSound(5);
-                    },
-                    color: Colors.teal,
-                    child: null,
-                  ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  color: Colors.teal,
+                  child: null,
                 ),
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      playSound(6);
-                    },
-                    color: Colors.lightBlue,
-                    child: null,
-                  ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  color: Colors.lightBlue,
+                  child: null,
                 ),
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      playSound(7);
-                    },
-                    color: Colors.purple,
-                    child: null,
-                  ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  color: Colors.purple,
+                  child: null,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
